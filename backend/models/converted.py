2fe76166de12a7ffc9f6c93f6ac86544f0bf97c0
kuladi03 <kuladi03@gmail.com>
2025-05-10 17:36:08 +0530

    updated app.py

--- backend/models/converted.py
+++ backend/models/converted.py
@@ -1,17 +1,4 @@
-from pymongo import MongoClient
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# Test connection
-try:
-    client = MongoClient(os.getenv("MongoURI"))
-    print("Connection successful")
-except Exception as e:
-    print("Error:", e)
-
-db = client["anvesha"]
-collection = db["students"]
-result = collection.find_one()  # Just fetch the first document
-print(result)
+print(os.getcwd())  # Print the current working directory
+print(os.path.exists('backend/models/dropout_risk_model.pkl'))  # Check if the file exists
+print(os.path.exists('backend/models/label_encoders.pkl'))  # Check if the file exists
